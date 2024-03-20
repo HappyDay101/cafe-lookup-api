9cc94f9886fcd88f01898b67276520cabbebb23f
Osama Suleiman <osama.suleiman97@gmail.com>
2024-03-20 23:11:51 +0900

    create criteria route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :cafes, only: [ :index, :create ]
+      resources :cafes, only: [:index, :create]
+      resources :criteria, only: [:index]
     end
   end
 end
